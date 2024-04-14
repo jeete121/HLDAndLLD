8ca36684980664aa8403c26a3532eeee259c8761
Jitendra Kumar <jeete121@gmail.com>
2024-04-14 13:18:04 +0530

    added coding

--- LLD/mongo-jpa/src/main/java/com/example/mongo/repo/MongoRepositoryJPA.java
+++
@@ -1,9 +0,0 @@
-package com.example.mongo.repo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.mongo.model.User;
-
-public interface MongoRepositoryJPA extends MongoRepository<User,Integer>{
-
-}
