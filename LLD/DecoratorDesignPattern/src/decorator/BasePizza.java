a2fb090f7201e6e0e5a81b1de6593ed9526bccb2
jeete121 <60791491+jeete121@users.noreply.github.com>
2024-04-21 22:30:40 +0530

    Delete LLD/DecoratorDesignPattern directory

--- LLD/DecoratorDesignPattern/src/decorator/BasePizza.java
+++
@@ -1,7 +0,0 @@
-package decorator;
-
-public abstract class BasePizza {
-
-	public abstract int cost();
-
-}
--- LLD/DecoratorDesignPattern/src/decorator/ToppingDecorator.java
+++
@@ -1,5 +0,0 @@
-package decorator;
-
-public abstract class ToppingDecorator extends BasePizza {
-
-}
